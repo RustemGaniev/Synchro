eb5619572a0da5c9bbb63aaab027a8100ed08189
RustemGaniev <ganievrr@gmail.com>
2021-09-20 15:46:51 +0300

    Synchro

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,15 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        long SHOP_SRVICE_TIME = 1000;
         Shop shop = new Shop();
         String[] threadName = {"Покупатель 1", "Покупатель 2", "Покупатель 3", "Покупатель 4", "Покупатель 5",
                 "Покупатель 6", "Покупатель 7", "Покупатель 8", "Покупатель 9", "Покупатель 10"};
         for (String tN : threadName) {
             new Thread(shop :: buyCar, tN).start();
+            Thread.sleep(SHOP_SRVICE_TIME);
         }
         new Thread(shop :: productionCar, " Автосалон ").start();
     }
