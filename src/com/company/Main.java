c6dfa457787c4a407cf814389943462530c7f7cc
RustemGaniev <ganievrr@gmail.com>
2021-09-23 21:02:44 +0300

    Second Philipp Edition

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,15 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        long SHOP_SRVICE_TIME = 1000;
+        long SHOP_SERVICE_TIME = 1000;
         Shop shop = new Shop();
         String[] threadName = {"Покупатель 1", "Покупатель 2", "Покупатель 3", "Покупатель 4", "Покупатель 5",
                 "Покупатель 6", "Покупатель 7", "Покупатель 8", "Покупатель 9", "Покупатель 10"};
-        for (String tN : threadName) {
-            new Thread(shop :: buyCar, tN).start();
-            Thread.sleep(SHOP_SRVICE_TIME);
+        for (String tN:threadName) {
+            new Thread(shop::buyCar, tN).start();
+            Thread.sleep(SHOP_SERVICE_TIME);
+            new Thread(shop::productionCar, " Автосалон ").start();
         }
-        new Thread(shop :: productionCar, " Автосалон ").start();
+
     }
 }
--- src/com/company/Shop.java
+++ src/com/company/Shop.java
@@ -2,54 +2,42 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Shop {
 
-    int quantityOfCars = 10;
+    int CARS_COUNT = 10;
     long BUYER_WAIT_TIME = 1000;
     long TOYOTA_PRODUCTION_TIME = 2000;
 
-    Lock seller = new ReentrantLock(true);
-    List<Car> cars = new ArrayList<>(quantityOfCars);
-    Condition carCondition = seller.newCondition();
+    List<Car> cars = new ArrayList<>(CARS_COUNT);
 
-    public void buyCar() {
-            seller.lock();
-            try {
-                System.out.println("\n" + Thread.currentThread().getName() + " зашел в автосалон купить Toyota");
-                while (cars.isEmpty())
-                    try {
-                        System.out.println(" Автосалон просит подождать поставку машин");
-                        Thread.sleep(BUYER_WAIT_TIME);
-                        carCondition.await();
-                        System.out.println(" Говорят в автосалоне появились новенькие машины");
-                    } catch (InterruptedException err) {
-                    }
-                System.out.println(Thread.currentThread().getName() + " уехал на новенькой Toyota ");
-            } finally {
-                seller.unlock();
+    public synchronized void buyCar() {
+
+        try {
+            System.out.println("\n" + Thread.currentThread().getName() + " зашел в автосалон купить Toyota");
+            while (cars.isEmpty()) {
+                wait();
+                Thread.sleep(BUYER_WAIT_TIME);
             }
-        }
-
-
-    public void productionCar() {
-        seller.lock();
-        System.out.println(" Завод Toyota отгружает автомобиль в салон ");
-            try {
-                Thread.sleep(TOYOTA_PRODUCTION_TIME);
-                cars.add(new Car());
-                System.out.println(" Завод Toyota поставил один автомобиль в автосалон");
-                carCondition.signalAll();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                seller.unlock();
-            }
+            System.out.println(Thread.currentThread().getName() + " уехал на новенькой Toyota ");
+            cars.remove(0);
+        } catch (InterruptedException err) {
+            err.printStackTrace();
         }
     }
+
+    public synchronized void productionCar() {
+        System.out.println(" Завод Toyota отгружает автомобиль в салон ");
+        try {
+            cars.add(new Car());
+            System.out.println(" Завод Toyota поставил один автомобиль в автосалон");
+            notify();
+            Thread.sleep(TOYOTA_PRODUCTION_TIME);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
 
 
