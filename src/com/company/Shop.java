53ce75d85b11576a417a1a14486caa42ca7dcde3
RustemGaniev <ganievrr@gmail.com>
2021-09-27 12:41:04 +0300

    Synhcro

--- src/com/company/Shop.java
+++ src/com/company/Shop.java
@@ -5,40 +5,44 @@ import java.util.List;
 
 public class Shop {
 
-    int CARS_COUNT = 10;
-    long BUYER_WAIT_TIME = 1000;
-    long TOYOTA_PRODUCTION_TIME = 2000;
-    int FIRST_CAR_TO_BUY = 0;
+    static final int CARS_COUNT = 10;
+    static final long TOYOTA_PRODUCTION_TIME = 2000;
+    static final int FIRST_CAR_TO_BUY = 0;
 
     List<Car> cars = new ArrayList<>(CARS_COUNT);
 
-    public synchronized void buyCar() {
+    public void buyCar() {
 
         try {
             System.out.println("\n" + Thread.currentThread().getName() + " зашел в автосалон купить Toyota");
-            while (cars.isEmpty()) {
+             synchronized (this){
+                 while (cars.isEmpty()) {
                 wait();
-                Thread.sleep(BUYER_WAIT_TIME);
             }
             System.out.println(Thread.currentThread().getName() + " уехал на новенькой Toyota ");
             cars.remove(FIRST_CAR_TO_BUY);
+        }
         } catch (InterruptedException err) {
             err.printStackTrace();
         }
     }
 
-    public synchronized void productionCar() {
-        System.out.println(" Завод Toyota отгружает автомобиль в салон ");
-        try {
-            cars.add(new Car());
-            System.out.println(" Завод Toyota поставил один автомобиль в автосалон");
-            notify();
-            Thread.sleep(TOYOTA_PRODUCTION_TIME);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public void productionCar() {
+
+            System.out.println(" Завод Toyota отгружает автомобиль в салон ");
+            try {
+                cars.add(new Car());
+                synchronized (this) {
+                    System.out.println(" Завод Toyota поставил один автомобиль в автосалон");
+                    notify();
+                }
+                Thread.sleep(TOYOTA_PRODUCTION_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
-}
+
 
 
 
