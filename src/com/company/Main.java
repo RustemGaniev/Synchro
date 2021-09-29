d83529d35fd2bc106d2cd1d42e2a86847ebec7d4
RustemGaniev <ganievrr@gmail.com>
2021-09-29 10:08:39 +0300

    Fourth Edition without sleep out of thred

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,12 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        long SHOP_SERVICE_TIME = 1000;
         final Shop shop = new Shop();
         String[] threadName = {"Покупатель 1", "Покупатель 2", "Покупатель 3", "Покупатель 4", "Покупатель 5",
                 "Покупатель 6", "Покупатель 7", "Покупатель 8", "Покупатель 9", "Покупатель 10"};
-        for (String tN:threadName) {
-            new Thread(shop::buyCar, tN).start();
-            Thread.sleep(SHOP_SERVICE_TIME);
-            new Thread(shop::productionCar, " Автосалон ").start();
+        for (String tN : threadName) {
+            new Thread(shop :: buyCar, tN).start();
+            new Thread(shop :: productionCar, " Автосалон ").start();
         }
     }
 }
--- src/com/company/Shop.java
+++ src/com/company/Shop.java
@@ -7,6 +7,7 @@ public class Shop {
 
     static final int CARS_COUNT = 10;
     static final long TOYOTA_PRODUCTION_TIME = 2000;
+    static final int SHOP_SERVICE_TIME = 500;
     static final int FIRST_CAR_TO_BUY = 0;
 
     List<Car> cars = new ArrayList<>(CARS_COUNT);
@@ -15,13 +16,14 @@ public class Shop {
 
         try {
             System.out.println("\n" + Thread.currentThread().getName() + " зашел в автосалон купить Toyota");
-             synchronized (this){
-                 while (cars.isEmpty()) {
-                wait();
+            Thread.sleep(SHOP_SERVICE_TIME);
+            synchronized (this) {
+                while (cars.isEmpty()) {
+                    wait();
+                }
+                System.out.println(Thread.currentThread().getName() + " уехал на новенькой Toyota ");
+                cars.remove(FIRST_CAR_TO_BUY);
             }
-            System.out.println(Thread.currentThread().getName() + " уехал на новенькой Toyota ");
-            cars.remove(FIRST_CAR_TO_BUY);
-        }
         } catch (InterruptedException err) {
             err.printStackTrace();
         }
@@ -29,19 +31,19 @@ public class Shop {
 
     public void productionCar() {
 
-            System.out.println(" Завод Toyota отгружает автомобиль в салон ");
-            try {
-                cars.add(new Car());
-                synchronized (this) {
-                    System.out.println(" Завод Toyota поставил один автомобиль в автосалон");
-                    notify();
-                }
-                Thread.sleep(TOYOTA_PRODUCTION_TIME);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        System.out.println(" Завод Toyota отгружает автомобиль в салон ");
+        try {
+            cars.add(new Car());
+            synchronized (this) {
+                System.out.println(" Завод Toyota поставил один автомобиль в автосалон");
+                notify();
             }
+            Thread.sleep(TOYOTA_PRODUCTION_TIME);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
+}
 
 
 
